1a56e82d9bec32f743c76acbd2bf0e48c3f0d089
achester88 <anthonychester71@gmail.com>
2025-05-05 17:52:49 -0400

    Mon May  5 05:52:49 PM EDT 2025

--- backup.sh
+++ backup.sh
@@ -4,7 +4,7 @@ cp --parents -r /home/achester88/.config/kitty/ ./fedroa-hyperland-config
 cp --parents -r /home/achester88/.config/nvim/init.lua ./fedroa-hyperland-config/
 cp --parents -r /home/achester88/.config/wofi/ ./fedroa-hyperland-config/
 cp --parents -r /home/achester88/.config/starship.toml ./fedroa-hyperland-config/
-
+cp --parents -r /home/achester88/.config/fastfetch/ ./fedroa-hyperland-config/
 git add .
 
 git commit -m "$(date)"
