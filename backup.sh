cfca590cdafffa01d34233c0e1833071c6659a6b
achester88 <anthonychester71@gmail.com>
2025-05-05 03:13:25 -0400

    init

---
+++ backup.sh
@@ -0,0 +1,8 @@
+cp --parents -r /home/achester88/.config/waybar/config ./fedroa-hyperland-config
+cp --parents -r /home/achester88/.config/hypr ./fedroa-hyperland-config/
+cp --parents -r /home/achester88/.config/kitty/ ./fedroa-hyperland-config
+cp --parents -r /home/achester88/.config/nvim/init.lua ./fedroa-hyperland-config/
+cp --parents -r /home/achester88/.config/wofi/ ./fedroa-hyperland-config/
+cp --parents -r /home/achester88/.config/starship.toml ./fedroa-hyperland-config/
+
+
