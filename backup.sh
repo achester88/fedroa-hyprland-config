fec9ceb6298aa7d23a02fb6565f7b26bcc7b4062
achester88 <anthonychester71@gmail.com>
2025-05-05 13:24:22 -0400

    Mon May  5 01:24:22 PM EDT 2025

--- backup.sh
+++ backup.sh
@@ -1,4 +1,4 @@
-cp --parents -r /home/achester88/.config/waybar/config ./fedroa-hyperland-config
+cp --parents -r /home/achester88/.config/waybar/ ./fedroa-hyperland-config
 cp --parents -r /home/achester88/.config/hypr ./fedroa-hyperland-config/
 cp --parents -r /home/achester88/.config/kitty/ ./fedroa-hyperland-config
 cp --parents -r /home/achester88/.config/nvim/init.lua ./fedroa-hyperland-config/
